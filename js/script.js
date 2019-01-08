0e5b862a5ed786728a05c29e21d5dc694170db7e
Cartomu Kabba <cartomukabba123@gmail.com>
2019-01-08 22:50:53 +0000

    1/8/19

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 /* global $ */
 
 $(document).ready(function() {
+    
+    function q1Score(q1Answer) {
+        if (q1Answer === "walk") {
+            return 1;
+        }else {
+            return 2;
+        }
+        }
+    }
+    
     $("button").click(function() {
         var name = $("#name").val();
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
+        var q1Num = q1Score(q1Result);
+      if{ #question = "Walk"
+        return 1;
+      }
+        
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
     });
 
 });
+
